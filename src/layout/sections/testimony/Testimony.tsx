6ccfa9324f644ac6bf400817f8c659784aa1ac96
Dmitry Naumov <komrad.nda@yandex.ru>
2024-03-07 00:33:14 +0300

    Add Adaptive Testimony

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
-import {SectionTitle} from '../../../components/SectionTitle';
-import {Icon} from '../../../components/icon/Icon';
-import {Slider} from '../../../components/slider/Slider';
-import {FlexWrapper} from '../../../components/FlexWrapper';
-import {IconWrapper} from '../skills/skill/Skill';
+import { SectionTitle } from '../../../components/SectionTitle';
+import { Icon } from '../../../components/icon/Icon';
+import { Slider } from '../../../components/slider/Slider';
+import { FlexWrapper } from '../../../components/FlexWrapper';
+import { IconWrapper } from '../skills/skill/Skill';
+import { Container } from '../../../components/Container';
 
 export const Testimony = () => {
 	return (
-			<StyledTestimony>
+		<StyledTestimony>
+			<Container>
 				<SectionTitle>Testimony</SectionTitle>
 				<FlexWrapper direction={'column'} align={'center'}>
 					<IconWrapper>
-						<Icon iconId={'quote'}/>
+						<Icon iconId={'quote'} />
 					</IconWrapper>
-					<Slider/>
+					<Slider />
 				</FlexWrapper>
-			</StyledTestimony>
+			</Container>
+		</StyledTestimony>
 	);
 };
 
@@ -27,4 +30,4 @@ const StyledTestimony = styled.section`
 	${IconWrapper} {
 		margin: 40px 0 70px;
 	}
-`
+`;
