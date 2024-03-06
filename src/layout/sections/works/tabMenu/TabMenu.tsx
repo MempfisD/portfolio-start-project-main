2aeb2e8d837c1b0afc3af5628ee0271dc34090f2
Dmitry Naumov <komrad.nda@yandex.ru>
2024-03-07 00:24:03 +0300

    Add Adaptive Works

--- src/layout/sections/works/tabMenu/TabMenu.tsx
+++ src/layout/sections/works/tabMenu/TabMenu.tsx
@@ -1,32 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
-import {Link} from '../../../../components/Link';
-
+import { Link } from '../../../../components/Link';
 
 export const TabMenu = (props: { menuItems: Array<string> }) => {
 	return (
-			<StyledTabMenu>
-				<ul>
-					{props.menuItems.map((item, index) => {
-						return <ListItem key={index}>
-							<Link href="">{item}</Link>
+		<StyledTabMenu>
+			<ul>
+				{props.menuItems.map((item, index) => {
+					return (
+						<ListItem key={index}>
+							<Link href=''>{item}</Link>
 						</ListItem>
-					})}
-				</ul>
-			</StyledTabMenu>
+					);
+				})}
+			</ul>
+		</StyledTabMenu>
 	);
 };
 
 const StyledTabMenu = styled.nav`
-	margin-bottom: 60px;
-
 	ul {
 		display: flex;
-		gap: 20px;
-		justify-content: center;
-
+		justify-content: space-between;
+		max-width: 352px;
+		width: 100%;
+		margin: 0 auto 40px;
 	}
-`
-const ListItem = styled.li`
-`
-
+`;
+const ListItem = styled.li``;
--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -8,12 +8,12 @@ export const theme = {
 
 		font: '#FFFFFF',
 
-		placeholderColor: '#495057'
+		placeholderColor: '#495057',
 	},
 
 	media: {
 		tablet: 'screen and (max-width: 768px)',
 		mobile: 'screen and (max-width: 576px)',
-	}
-}
-
+		desktop: 'screen and (min-width: 1140px)',
+	},
+};
