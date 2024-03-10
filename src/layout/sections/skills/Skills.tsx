00cada03e26525348d4039b941d80d7086a9a42e
Dmitry Naumov <komrad.nda@yandex.ru>
2024-03-10 10:45:45 +0300

    refactoring skill

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -1,52 +1,67 @@
 import React from 'react';
-import styled from 'styled-components';
-import {FlexWrapper} from '../../../components/FlexWrapper';
-import {SectionTitle} from '../../../components/SectionTitle';
-import {Skill} from './skill/Skill';
-import {Container} from '../../../components/Container';
+import { FlexWrapper } from '../../../components/FlexWrapper';
+import { SectionTitle } from '../../../components/SectionTitle';
+import { Skill } from './skill/Skill';
+import { Container } from '../../../components/Container';
+import { S } from './Skill_styles';
+
+const skillData = [
+	{
+		iconId: 'code',
+		title: 'HTML5',
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim',
+	},
+	{
+		iconId: 'css',
+		title: 'CSS3',
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim',
+	},
+	{
+		iconId: 'react',
+		title: 'REACT',
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim',
+	},
+	{
+		iconId: 'typeScript',
+		title: 'TYPESCRIPT',
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim',
+	},
+	{
+		iconId: 'styledComponents',
+		title: 'styled components',
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim',
+	},
+	{
+		iconId: 'figma',
+		title: 'WEB DESIGN',
+		description:
+			'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim',
+	},
+];
 
 export const Skills = () => {
 	return (
-			<StyledSkills>
-				<Container>
-					<SectionTitle>My Skills</SectionTitle>
-					<FlexWrapper wrap={'wrap'} justify={'space-between'}>
-						<Skill
-								iconId={'code'}
-								title={'HTML5'}
-								description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
-						></Skill>
-						<Skill
-								iconId={'css'}
-								title={'CSS3'}
-								description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
-						></Skill>
-						<Skill
-								iconId={'react'}
-								title={'REACT'}
-								description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
-						></Skill>
-						<Skill
-								iconId={'typeScript'}
-								title={'TYPESCRIPT'}
-								description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
-						></Skill>
-						<Skill
-								iconId={'styledComponents'}
-								title={'styled components'}
-								description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
-						></Skill>
-						<Skill
-								iconId={'figma'}
-								title={'WEB DESIGN'}
-								description="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim"
-						></Skill>
-					</FlexWrapper>
-				</Container>
-			</StyledSkills>
+		<S.Skills>
+			<Container>
+				<SectionTitle>My Skills</SectionTitle>
+				<FlexWrapper wrap={'wrap'} justify={'space-between'}>
+					{skillData.map((s, index) => {
+						return (
+							<Skill
+								iconId={s.iconId}
+								key={index}
+								title={s.title}
+								description={s.description}
+							/>
+						);
+					})}
+				</FlexWrapper>
+			</Container>
+		</S.Skills>
 	);
 };
-
-const StyledSkills = styled.section`
-
-`;
